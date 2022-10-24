ba526c687e11109cde6a6ba61527d1d503d72f36
herisusantoarismann <heri.s.arisman@gmail.com>
2022-10-24 11:14:07 +0700

    Add bisuness section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+interface IProps {
+  styles?: string;
+}
+
+const Button = ({ styles }: IProps) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
 };
 
 export default Button;
