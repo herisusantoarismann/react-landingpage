2394c0b1e3e44dde7c1f4e5297b4ae5c617e4f78
herisusantoarismann <heri.s.arisman@gmail.com>
2022-10-24 10:08:48 +0700

    Add stucture & layout

---
+++ src/components/Bisuness.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Bisuness = () => {
+  return <div>Bisuness</div>;
+};
+
+export default Bisuness;
---
+++ src/components/FeedbackCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FeedbackCard = () => {
+  return <div>FeedbaCard</div>;
+};
+
+export default FeedbackCard;
